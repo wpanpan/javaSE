3540f8fe5be110d0852b4bba49ce306207657ebf
wpanpan <1178582642@qq.com>
2017-04-18 22:14:59 +0800

    Create achieveAbsClass.java

---
+++ achieveAbsClass.java
@@ -0,0 +1,14 @@
+public class achieveAbsClass extends absClass{
+	private int cnt;
+	public int w;
+	public achieveAbsClass() {
+		
+	}
+	//实现继承过来的抽象方法
+	public void absFun1() {
+		
+	}
+	public void absFun2(int a,int b) {
+		
+	}
+}
